0f9526cbfafeb375e28475c469fb5cbc6768dcb2
kflange <zknsftk1msrk@gmail.com>
2017-11-03 10:30:16 +0900

    init

---
+++ src/script/cli_10/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+import (
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "boom"
+	app.Usage = "make an explosive entrance"
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
+	}
+
+	app.Run(os.Args)
+}
